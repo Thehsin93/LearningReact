5977975fe69abb0437d836a56dc31d9dbb5b2fb3
Thehsin93 <thehsinnew@gmail.com>
2024-04-23 17:07:18 +0400

    Reactep5

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
-const heading = React.createElement("h1",{},"hello world");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import mainLogo from'./user.jpg';
+import companylogo from "./logo.jpg";
+const Input = () =>  ( <input type="text"
+className="textbox"
+placeholder="Search data...">
+</input>)
+
+const Heading =()=>(<h1> <img  src={companylogo} className="UserIcon" alt="fireSpot2"/>
+    {<Input/>}
+     <img  src={mainLogo} className="UserIcon" alt="fireSpot"/>
+</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<Heading/>);
